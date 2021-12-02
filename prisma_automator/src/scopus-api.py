fce5b50695e49f12e79fdd6dbb638ee4b355b845
Fabiano Manschein <fabianomanschein2@gmail.com>
2021-12-02 17:12:01 +0100

    Remove legacy code

--- prisma_automator/src/scopus-api.py
+++
@@ -1,4 +0,0 @@
-from pybliometrics.scopus import ScopusSearch
-search = "TITLE-ABS-KEY(\"Digital Twin\" AND \"Assembly\")"
-s = ScopusSearch(search, subscriber=False, download=False)
-print(s.get_results_size())
