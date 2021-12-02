ae0404fd6ce17af6b7a7c9326930cc50755937c3
Fabiano Manschein <fabianomanschein2@gmail.com>
2021-12-02 17:16:55 +0100

    Add tests for the Collector class

--- test_suite.py
+++ test_suite.py
@@ -1,8 +1,10 @@
 import pytest
+
+from prisma_automator.collector import Collector
 from prisma_automator.splitter import Splitter
 
 
-class TestSplitter():
+class TestSplitter:
     @pytest.fixture
     def create_splitter(self):
         self.splitter = Splitter()
@@ -100,3 +102,88 @@ class TestSplitter():
         self.splitter.add_kwgroups(self.kw_groups)
         splits = self.splitter.split(log=False, save_to="")
         assert sorted(splits) == sorted(expected_splits)
+
+
+class TestCollector:
+    """  Tests for the Collector class
+
+    - NOTE: these tests take longer to complete because it has API calls. The results
+    should be saved to memory after the first run.
+    """
+    @pytest.fixture
+    def create_collector_and_splits(self):
+        """
+
+        The chosen splits return at least 1 result, but always very few, so that
+        the search can be executed quickly. It's known that the number of results
+        can change in the future, but this is the best way found to test the
+        Collector's methods. As of 02.12.21, total results are 2 + 2 + 23 = 27.
+
+        - NOTE: there's a repeated split for the sake of testing the remove duplicates
+        functionality of the screen() method.
+
+        """
+
+        self.splits = [
+            "\"Digital Twin\" AND \"Augmented Reality\"",
+            "\"Digital Twin\" AND \"Augmented Reality\"",
+            "\"Virtual Reality\"  AND  \"BCI\"  AND  \"Gaming\""
+        ]
+        self.collector = Collector()
+
+    def test_search_results_df_has_all_expected_columns(self, create_collector_and_splits):
+        # NOTE: "splits" column is added by the search method.
+        expected_columns = ['splits', 'eid', 'doi', 'pii', 'pubmed_id', 'title', 'subtype',
+                            'subtypeDescription', 'creator', 'afid', 'affilname',
+                            'affiliation_city', 'affiliation_country', 'author_count',
+                            'author_names', 'author_ids', 'author_afids', 'coverDate',
+                            'coverDisplayDate', 'publicationName', 'issn', 'source_id', 'eIssn',
+                            'aggregationType', 'volume', 'issueIdentifier', 'article_number',
+                            'pageRange', 'description', 'authkeywords', 'citedby_count',
+                            'openaccess', 'fund_acr', 'fund_no', 'fund_sponsor']
+        all_results = self.collector.search(
+            splits=self.splits, subscriber=False, download=True, threshold=1000, log=False)
+        df = all_results[0]
+        assert all(column in df for column in expected_columns) and len(
+            df.columns) == len(expected_columns)
+
+    def test_screen_dropped_unnecessary_columns(self, create_collector_and_splits):
+        expected_columns = ['splits', 'doi', 'title', 'subtypeDescription', 'creator',
+                            'author_names', 'coverDate', 'volume', 'issueIdentifier', 'pageRange',
+                            'description', 'authkeywords', 'citedby_count', 'openaccess']
+        all_results = self.collector.search(
+            splits=self.splits, subscriber=False, download=True, threshold=1000, log=False)
+        df = all_results[0]
+        screened_df = self.collector.screen(df=df, log=False)
+        assert all(column in screened_df for column in expected_columns) and len(
+            screened_df.columns) == len(expected_columns)
+
+    def test_screen_removed_conference_reviews(self, create_collector_and_splits):
+        all_results = self.collector.search(
+            splits=self.splits, subscriber=False, download=True, threshold=1000, log=False)
+        df = all_results[0]
+        screened_df = self.collector.screen(df=df, log=False)
+        assert 'Conference Review' not in screened_df.subtypeDescription.unique()
+
+    def test_screen_removed_duplicates(self, create_collector_and_splits):
+        """
+        Duplicates in this test are checked with both "doi" and "title" columns.
+        """
+        all_results = self.collector.search(
+            splits=self.splits, subscriber=False, download=True, threshold=1000, log=False)
+        df = all_results[0]
+        screened_df = self.collector.screen(df=df, log=False)
+        num_duplicates = screened_df.duplicated(subset=['doi', 'title']).sum()
+        assert num_duplicates == 0
+
+    def test_screen_removed_rows_without_doi(self, create_collector_and_splits):
+        """
+        NOTE: test needs to be improved! It'll always pass for the current splits.
+        TODO: instead of using collector.search() to get the dataframe, create
+        the dataframe manually and then screen it.
+        """
+        all_results = self.collector.search(
+            splits=self.splits, subscriber=False, download=True, threshold=1000, log=False)
+        df = all_results[0]
+        screened_df = self.collector.screen(df=df, log=False)
+        assert '' not in screened_df.doi.unique()
